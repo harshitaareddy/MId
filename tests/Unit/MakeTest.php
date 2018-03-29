8391224237d37c7c1c69d7aa4e29500307e343a0
harshita <hk389@njit.edu>
2018-03-29 03:08:59 -0400

    Created a unit test to check if a car's make is either ford / honda / toyota.

---
+++ tests/Unit/MakeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class MakeTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $car = Car::inRandomOrder()->first();
+        $value=$car->make;
+        $this->assertContains($value, ['honda','toyota','ford']);
+    }
+}
